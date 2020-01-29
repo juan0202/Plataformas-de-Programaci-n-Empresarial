dfe7251c89adcf4e618046668b67a144e0773b8a
juan0202 <44534358+juan0202@users.noreply.github.com>
2020-01-29 11:38:06 -0500

    Update NumerosPrimos.cs

--- NumerosPrimos.cs
+++ NumerosPrimos.cs
@@ -13,69 +13,44 @@ namespace Ejercicio1Plataformas
             int[] vec1 = new int[4000];
             generarPrimos(vec1);
             Imprimir(vec1);
-
-
-
-
         }
 
         public static void generarPrimos(int[] vec1)
         {
-
             int cont1 = 1, cont2 = 0;
             bool bandera = false;
 
             for (int i = 0; i < vec1.Length; i++)
             {
                 cont1++;
-
-
                 cont2 = 0;
                 for (int j = 1; j < cont1; j++)
                 {
-
-
-
                     if (cont1 % j == 0)
                     {
                         cont2++;
                     }
-
-                    
-
-                    
                 }
-
-
-               
+                
                 if (cont2 == 1)
                 {
-                    vec1[i] = cont1;
-
+                   vec1[i] = cont1;
                 }
-
-
-
             }
         }
 
    
-
         public static void Imprimir(int[] vec1)
         {
-           
             for(int i = 0; i < vec1.Length; i++)
             {
-              
                     if (vec1[i] != 0)
                     {
-                    
                         Console.WriteLine("El numero primo " + i  + " es " + vec1[i]);
                         Console.ReadKey();
                     }
-                
-             
             }
+            
         }
        
     }
